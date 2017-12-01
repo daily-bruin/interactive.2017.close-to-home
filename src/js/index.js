2a74d7536faf3b5ca686630947c2969913ff7c46
Miranda <miratang@gmail.com>
2017-12-01 11:42:56 -0800

    Added fade in and fade out effect for menubar

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,22 @@ function setTitlePhotoHeight() {
   $('.title-photo').height($(window).height());
 }
 
+var topofDiv = $(".title-photo").offset().top;
+var height = $(".title-photo").outerHeight();
+
+(function($) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > (topofDiv + height)) {
+                $('.top-bar').fadeIn(500);
+                $('.initial-top-bar').fadeOut(500);
+            } else {
+                $('.top-bar').fadeOut(500);
+                $('.initial-top-bar').fadeIn(500);
+            }
+        });
+    });
+})(jQuery);
 // function hid
 
 setTitlePhotoHeight();
