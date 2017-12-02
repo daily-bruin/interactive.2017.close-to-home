906b21f277301f5979644dc1c1afcda10f6483c4
Miranda <miratang@gmail.com>
2017-12-02 14:46:56 -0800

    Added scroll then fix menubar

--- src/js/index.js
+++ src/js/index.js
@@ -6,21 +6,16 @@ function setTitlePhotoHeight() {
   $('.title-photo').height($(window).height());
 }
 
-var topofDiv = $(".title-photo").offset().top;
-var height = $(".title-photo").outerHeight();
-
-(function($) {
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > (topofDiv + height)) {
-                $('.top-bar').fadeIn(500);
-                $('.initial-top-bar').fadeOut(500);
-            } else {
-                $('.top-bar').fadeOut(500);
-                $('.initial-top-bar').fadeIn(500);
-            }
-        });
+(function fadeBar() {
+  $(document).ready(() => {
+    $(window).scroll(function scrollEffects() {
+      if ($(this).scrollTop() >= $(window).height()) {
+        $('.top-bar').addClass('fix-bar');
+      } else {
+        $('.top-bar').removeClass('fix-bar');
+      }
     });
+  });
 })(jQuery);
 // function hid
 
