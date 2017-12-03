51d93e0c93de38b6da4f2f2ea43e02684b47ec5a
Miranda Tang <miratang@gmail.com>
2017-12-02 20:07:33 -0800

    Added menubar (#15)
    * added menubar
    
    * added menubar
    
    * Added fade in and fade out effect for menubar
    
    * no .DS_Store
    
    * Added scroll then fix menubar
    
    * no .orig
    
    * Links

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,19 @@ function setTitlePhotoHeight() {
   $('.cover-photo').height($(window).height());
 }
 
+(function fadeBar() {
+  $(document).ready(() => {
+    $(window).scroll(function scrollEffects() {
+      if ($(this).scrollTop() >= $(window).height()) {
+        $('.top-bar').addClass('fix-bar');
+      } else {
+        $('.top-bar').removeClass('fix-bar');
+      }
+    });
+  });
+})(jQuery);
+// function hid
+
 setTitlePhotoHeight();
 
 $(window).resize(setTitlePhotoHeight);
