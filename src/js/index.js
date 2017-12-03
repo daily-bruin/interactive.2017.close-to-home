80b9dbf06553860e389897135534665168f9a5a5
Richard <richbluehill@gmail.com>
2017-12-03 12:43:22 -0800

    fixed cover photo js deleting

--- src/js/index.js
+++ src/js/index.js
@@ -239,12 +239,37 @@ $(document).ready(() => {
 hello();
 
 function setTitlePhotoHeight() {
-  $('.title-photo').height($(window).height());
+  $('.cover-photo').height($(window).height());
 }
+
+(function fadeBar() {
+  $(document).ready(() => {
+    $(window).scroll(function scrollEffects() {
+      if ($(this).scrollTop() >= $(window).height()) {
+        $('.top-bar').addClass('fix-bar');
+      } else {
+        $('.top-bar').removeClass('fix-bar');
+      }
+    });
+  });
+})(jQuery);
 
 // function hid
 
 setTitlePhotoHeight();
 
 $(window).resize(setTitlePhotoHeight);
+$(window).waypoint(() => {
+  $('.cover-photo__header').fadeIn();
+  $('.db-logo').fadeIn();
+});
+
+$(window).waypoint(
+  () => {
+    $('.cover-photo__header').fadeOut();
+    $('.db-logo').fadeOut();
+  },
+  { offset: -1 }
+);
+
 $(window).scroll();
